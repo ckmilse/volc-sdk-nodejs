6554134faf76a4d75ee236d2aa20537b711303a7
chenke.7knives <chenke.7knives@bytedance.com>
2021-08-24 17:26:20 +0800

    fix-temp

--- test/acep.test.js
+++ test/acep.test.js
@@ -1,9 +1,18 @@
-import { acep } from "../lib";
+import { Service } from "../lib";
 import { getListPodResultReponseSchemaValidate } from "./schema/acep";
+const defaultConfig = {
+  serviceName: "ACEP",
+  defaultVersion: "2020-10-25",
+};
+const ACEPService = new Service(defaultConfig);
 
 test("edit:listPodEditResult", async () => {
-  const response = await acep.GetListPod({
-    product_id: '1426095758716702720'
+  const response = await ACEPService.fetchOpenAPI({
+    Action: "ListPod",
+    Version: "2020-10-25",
+    query: {
+      product_id: '1426095758716702720',
+    }
   });
   const validateResult = getListPodResultReponseSchemaValidate(response);
   expect(validateResult).toBe(true);
